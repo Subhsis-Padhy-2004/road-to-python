af3847a3a582224adbfc94d5790beca5706ccd06
Subhsis-Padhy-2004 <subhsispadhy@gmail.com>
2026-04-28 23:41:49 +0530

    Ininitial commit

--- list_methods.py
+++ list_methods.py
@@ -2,6 +2,8 @@ friends = ["Apple", "Orange", 5 , 345.06, False, "Aakash", "Rohan"]
 print(friends)
 friends.append("Harry")
 print(friends)
+friends.remove("Orange")
+print(friends)
 
 l1 = [1, 34 ,62 ,2 ,6 ,11]
 # l1.sort()
@@ -12,4 +14,4 @@ l1 = [1, 34 ,62 ,2 ,6 ,11]
 
 value = l1.pop(3)
 print(value)
-print(l1)
+print(l1) 
---
+++ tuple.py
@@ -0,0 +1,3 @@
+a = (1,2,3,4,5)
+a = () #emty tuple
+print(type(a))
